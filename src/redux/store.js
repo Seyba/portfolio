a991ce6d585b659efd2375c4244c77e2f7cd2614
Mahamadou <mmohmo@yahoo.com>
2023-08-14 17:19:06 -0400

    redux folder removed

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore} from '@reduxjs/toolkit';
-import { contactsSlice} from './slices/contactsSlice'
-
-export default configureStore({
-    reducer: {
-        contacts: contactsSlice
-    }
-});
