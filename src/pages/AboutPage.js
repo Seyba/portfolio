83737fff7072cecaeaa64c62d9396fd796ebe5f6
Mahamadou <mmohmo@yahoo.com>
2023-08-17 12:22:21 -0400

    about page updated

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,19 +8,14 @@ export const AboutPage = () => {
     console.log(visitors)
     return(
         <div className="text-center max-auto">
-            {/** Small devices stuff */}
-            <div className="md:flex justify-around">
-                <div>One</div>
-                <div>Two</div>
-                <div>Three</div>
-            </div>
+            
             {/** summary section */}
             <section className="md:grid grid-cols-3 gap-3 mb-4">
                 <div className="bg-slate-200  rounded-3xl mb-4">Img section</div>
                 <div className="bg-slate-200 col-span-2 py-8 rounded-3xl mb-4">
                     <h3 className="text-4xl font-semibold">Mahamadou Tirera</h3>
                     <p>
-                        I am a New York City based Full Stack Web Developer with a focus on web design, illustration, a visual development. I have a wide range of tools in my developer tool box and always using the latest in the industry.
+                        I am a New York City based Full Stack Web Developer with a focus on web design, illustration, a visual development.  I have a diverse range of experience having worked across various fields and industries.
                         I have professional experience in customer service, in teaching and hospitality.
                         I am proficient in JavaScript, html/CSS, React, Firebase, bootstrap, Sass and Tailwind CSS.
                         I'm currently working on a ReactJS and Firebase web project.
@@ -40,11 +35,7 @@ export const AboutPage = () => {
             </section>
             <div className="mx-4">            
                 <div className="w-1/2 bg-white  rounded-xl md:hidden lg:hidden">
-                    <img 
-                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" 
-                        alt="profile"
-                        className="w-60"
-                    />
+                    <h3>image</h3>
                 </div>
 
                 <h2 className="lg:hidden text-2xl font-bold">SELF SUMMARY</h2>
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -5,13 +5,42 @@ export const ContactPage = () => {
     return(
         <div>
             <section className="md:grid grid-cols-3 gap-3 mb-4">
-                <div className="bg-slate-200  rounded-3xl mb-4">
-                    <div>Email</div>
-                    <div>Phone</div>
-                    <div>Location</div>
+                <div className="bg-gray-100  py-8 rounded-3xl mb-4">
+                    <h3>CONTACT INFO</h3>
+                    <div>
+                        <h4>Email</h4>
+                        <div className="md:flex justify-around">
+                            <div className="bg-gray-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                </svg>
+                            </div>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                            </svg>
+
+
+                            <div>Email</div>
+                        </div>
+                        
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                            </svg>
+                        </div>
+                    </div>
                 </div>
-                    
-                <div className="bg-slate-100 col-span-2 py-8 rounded-3xl mb-4">
+                    {/*Contact form section */}
+                <div className="bg-gray-100 col-span-2 py-8 rounded-3xl mb-4">
                     <div className="flex justify-center">
                         
                         <div className="block w-3/4 rounded-3xl bg-white p-16 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
@@ -36,6 +65,7 @@ export const ContactPage = () => {
 
                                 </TEInput>
 
+                                {/* <!--Subject input--> */}
                                 <TEInput
                                     type="text"
                                     label="Subject"
@@ -74,7 +104,6 @@ export const ContactPage = () => {
                             </form>
                         </div>
                     </div>
-                    
                 </div>
             </section>
         </div>
