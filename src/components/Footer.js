e3c10cea15f18e76a6835ebfabf635af4b677244
Mahamadou <mmohmo@yahoo.com>
2023-10-10 22:27:06 -0400

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,31 @@
 export const Footer = () => {
     return(
         <footer>
-            <div className="mx-auto py-16 text-center" >
+            <div className="mx-auto py-16 " >
+                <div className="bg-slate-950 text-slate-50 md:grid grid-cols-4 gap-2">
+                    <div>
+                        <h2>Develop</h2>
+                        <p>Creating stunning websites for global enterprises. If you're seeking a contemporary and impactful online presence, reach out via email. If it's a match, I'll provide a comprehensive time and cost estimate.</p>
+                    </div>
+                    <div>
+                        <h2>Design</h2>
+                        <p>Unveil your brand's essence through a meticulous blend of imagery, messaging, and strategy. Crafting identities that resonate and endure.</p>
+                    </div>
+                    <div>
+                        <h2>ux</h2>
+                        <p>Designing experiences that captivate users from the first click. Intuitive interfaces that lead to seamless interactions, ensuring a memorable journey.</p>
+                    </div>
+                    <div>
+                        <h2>UI</h2>
+                        <p>
+                        Translating visions into dynamic digital solutions. From responsive websites to interactive applications, driving innovation through code and creativity.
+                        </p>
+                    </div>
+
+                </div>
                 <h2 className="text-3xl font-semibold ">Seyba</h2>
                 <p>© All rights reserved by SEYBA</p>
             </div>
-
         </footer>
     )
 }
